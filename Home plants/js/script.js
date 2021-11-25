92b3a2e6c9bf739191d745ce4d8fbca3d782221b
ArmorDs <zeleznoff84-00@mail.ru>
2021-11-25 22:27:25 +0300

    add news

--- Home plants/js/script.js
+++ Home plants/js/script.js
@@ -3,3 +3,19 @@ $(document).ready(function () {
 		$('.page__help-items, .header__menu').toggleClass('active');
 	});
 });
+
+$(function(){
+	const wrapper = $('#wrapper');
+	const contentRightInnerOffset = 
+	$('#content__right-inner').offset().top;
+
+	$(window).scroll(function(){
+		const scrolled = $(this).scrollTop();
+
+		if (scrolled > contentRightInnerOffset){
+
+		} else if(scrolled < contentRightInnerOffset){
+			
+		}
+	})
+});
